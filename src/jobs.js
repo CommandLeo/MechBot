a9eb3fca7466585183393f2227f9f0da707f9fc7
Crec0 <83436716+Crec0@users.noreply.github.com>
2022-06-29 10:11:00 -0700

    fix cron jobs for meetings

--- src/jobs.js
+++ src/jobs.js
@@ -2,14 +2,14 @@ import Schedule from 'node-schedule';
 import {client, MECHANIST_DATA} from "./index.js";
 
 export default async function jobs() {
-	Schedule.scheduleJob('0 16 * * 6', () => {
+	Schedule.scheduleJob('0 19 * * 0', () => {
 		const BOARD_CHANNEL = client.CHANNELS.BOARD;
 		const BOARD_ROLE = client.ROLES.BOARD;
 
-		BOARD_CHANNEL.send(`${BOARD_ROLE} meeting in 1 hour`);
+		BOARD_CHANNEL.send(`${BOARD_ROLE} meeting in 30 minutes!`);
 	});
 
-	Schedule.scheduleJob('0 17 * * 6', () => {
+	Schedule.scheduleJob('30 19 * * 0', () => {
 		const BOARD_CHANNEL = client.CHANNELS.BOARD;
 		const BOARD_ROLE = client.ROLES.BOARD;
 
